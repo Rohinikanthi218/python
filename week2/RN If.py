3fa93df5afc4840831ffa69ec8e766e2b1a5642e
Rohinikanthi218 <rohinikanthi16@gmail.com>
2025-03-28 12:12:14 +0530

    Update RN If.py

--- week2/RN If.py
+++ week2/RN If.py
@@ -1,12 +1,15 @@
 Q) Game
 import random
 n=random.randint(1,100)
+count=5
 while (True):
     x=int(input("enter the number:"))
     if x < n :
         print("Too low")
     elif x > n :
         print("Too high")
-    else:
+    else :    
         print("Congratulations! You got the correct number")
         break;
+        count = count - 1
+print("You lost!")        
