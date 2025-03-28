202e86b13b7a3291f30a14fe5f65a1c0a530b9c7
Rohinikanthi218 <rohinikanthi16@gmail.com>
2025-03-28 16:21:35 +0530

    Create not prime range.py

---
+++ week2/not prime range.py
@@ -0,0 +1,9 @@
+x=int(input("Enter number: "))
+for i in range(2,x):
+    flag=1
+    for j in range(2,i):
+        if (i%j == 0):
+            flag=0
+            break
+    if(flag == 0):
+       print(i,end=" ")
