c02887632f2f4bbddc7acd9d07fdf6589728b652
Rohinikanthi218 <rohinikanthi16@gmail.com>
2025-03-28 15:27:58 +0530

    Update fibonacci.py

--- week2/fibonacci.py
+++ week2/fibonacci.py
@@ -1,7 +1,7 @@
-def fibonacci(n):
-    a,b = 0, 1
-    for _ in range(n):
-        print(a, end=" ")
-        a, b = b, a + b
-n = int(input("Enter the number of terms: "))
-fibonacci(n)
+a=0
+b=1
+count=20
+while(count>0):
+    print(a, end=" ")
+    a,b=b,a+b
+    count=count-1
